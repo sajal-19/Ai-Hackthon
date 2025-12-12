7430c0e2d9e480a44628dcd8e3329da5bdb0f7a2
PLSV <plsv297@gmail.com>
2025-12-12 12:55:06 +0530

    minor package change

--- app/core/config.py
+++ app/core/config.py
@@ -1,11 +1,15 @@
 from functools import lru_cache
-from pydantic import BaseSettings, AnyUrl
+from pydantic import AnyUrl
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     ENVIRONMENT: str = "production"
     DATABASE_URL: AnyUrl
+
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str = "HS256"
+    JWT_ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
 
     class Config:
         env_file = ".env"
